f038780fd78aaf9f5ee61502521eebf649da153f
Matheus Rosa <matheusrosataxa@gmail.com>
2024-06-25 21:22:19 -0300

    feat: UserEntity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'email', nullable: false })
+  email: string;
+
+  @Column({ name: 'cpf', nullable: false })
+  cpf: string;
+
+  @Column({ name: 'password', nullable: false })
+  password: string;
+}
