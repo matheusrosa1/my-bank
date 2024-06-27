f96f8063cac415757b4199f1d07e58db8ba59b2a
Matheus Rosa <matheusrosataxa@gmail.com>
2024-06-27 11:18:25 -0300

    feat: payment module, payment service

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentEntity } from './entities/payment.entity';
+import { AccountEntity } from 'src/account/entities/account.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PaymentEntity, AccountEntity])],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { Repository } from 'typeorm';
+import { PaymentEntity } from './entities/payment.entity';
+import { AccountEntity } from 'src/account/entities/account.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PaymentService {
+  constructor(
+    @InjectRepository(PaymentEntity)
+    private readonly paymentRepository: Repository<PaymentEntity>,
+    @InjectRepository(AccountEntity)
+    private readonly accountRepository: Repository<AccountEntity>,
+  ) {}
+
   create(createPaymentDto: CreatePaymentDto) {
     return 'This action adds a new payment';
   }
